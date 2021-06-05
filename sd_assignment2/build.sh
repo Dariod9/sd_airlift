f90e04aba6d0a01b6f1a016fa3629a590d196dc0
Pedro Valente <pedro.valente@ua.pt>
2021-06-05 22:36:43 +0100

    start assignment 3

--- sd_assignment2/build.sh
+++ sd_assignment2/build.sh
@@ -1,3 +1,4 @@
+find -name "*.java" > sources.txt
 javac -cp "../genclass.jar" @sources.txt
 rm -f  dirAirLift.zip
 zip -rq dirAirLift.zip serverSide clientSide commInfra ../genclass.jar
---
+++ sd_assignment_3/serverSide/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *  Server side.
+ *
+ *  Air Lift application: code is moved and executed in a platform supposed to be more powerful.
+ *  Communication is based in Java RMI.
+ */
+
+package serverSide;
